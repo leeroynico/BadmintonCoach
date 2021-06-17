96d708ea6bf33e7199ffb97e63a3187b6c338b9a
leeroynico <73426065+leeroynico@users.noreply.github.com>
2021-06-17 22:10:57 +0200

    init commit

---
+++ components/Chronometer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Chronometer = () => {
+  return (
+    <View>
+      <Text>Mon chrono </Text>
+    </View>
+  );
+};
+
+export default Chronometer;
