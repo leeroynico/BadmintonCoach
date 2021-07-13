1576eb2bb916f25f0880b47b2445386b74588501
leeroynico <73426065+leeroynico@users.noreply.github.com>
2021-07-13 11:24:12 +0200

    features chrono & randow number ok (start and pause)

--- components/Home.js
+++ components/Home.js
@@ -1,19 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
 import StopWatch from "./StopWatch";
 import { useFonts } from "expo-font";
-import CircleAnimated from "./CircleAnimated";
+//import CircleAnimated from "./CircleAnimated";
 import { getRandom } from "./Logical";
 import { Speak } from "./speech";
-
+import { useInterval } from "./UseInterval";
+let chrono = null;
+let shadow = null;
 export default function Home(props) {
   const [loaded] = useFonts({
     Pangolin: require("../assets/fonts/Pangolin-Regular.ttf"),
   });
 
   const [icon, setIcon] = useState("play");
-  const [playState, setPlayState] = useState(false);
+  const [start, setStart] = useState("");
+  const [seconds, setSeconds] = useState(10);
+  const [randomNumber, setRandomNumber] = useState("");
+  const [delai, setDelai] = useState(2500);
+
+  const train = (number) => {
+    setRandomNumber(number);
+    Speak(number);
+  };
+
+  useInterval(() => {
+    if (start === "run") {
+      if (seconds > 0) {
+        train(getRandom(1, 7).toString());
+      }
+    } else {
+      ("");
+    }
+  }, delai);
+
+  useInterval(() => {
+    if (start === "run") {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      } else {
+        setSeconds("fin de la session");
+        setIcon("play");
+        setStart("");
+        Speak("c'est fini");
+      }
+    } else {
+      ("");
+    }
+  }, 1000);
 
   if (!loaded) {
     return null;
@@ -25,16 +60,31 @@ export default function Home(props) {
       <Button
         icon={icon}
         mode="contained"
+        style={styles.Button}
         onPress={() => {
-          Speak(getRandom(1, 7).toString());
-          setTimeout(function () {
-            alert("yo");
-          }, 2000);
-          icon === "play" ? setIcon("pause") : setIcon("play");
+          seconds === "fin de la session" ? setSeconds(10) : "";
+          if (start === "" || start === "pause") {
+            setStart("run");
+            setIcon("pause");
+          } else if (start === "run") {
+            setIcon("play");
+            setStart("pause");
+          }
         }}
       >
         {icon}
       </Button>
+
+      {/* <Button
+        icon="stop"
+        mode="contained"
+        color="orange"
+        onPress={() => {
+          clearTimeout(chrono);
+        }}
+      >
+        STOP
+      </Button> */}
       {/* <CircleAnimated /> */}
       <Image
         source={{
@@ -42,6 +92,8 @@ export default function Home(props) {
         }}
         style={{ width: 350, height: 350, marginTop: 10 }}
       />
+      <Text>{seconds}</Text>
+      <Text>{randomNumber}</Text>
     </View>
   );
 }
@@ -51,5 +103,8 @@ const styles = StyleSheet.create({
     fontFamily: "Pangolin",
     fontSize: 30,
     marginLeft: "20%",
+  },
+  Button: {
+    height: 40,
   },
 });
